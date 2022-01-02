464e095e145ca06fb4fa37d6b89514fd5bbd30d0
Thomas Coats <github@voodoolabs.net>
2022-01-03 08:13:00 +1300

    Upgrades

--- config.js
+++ config.js
@@ -1,16 +1,12 @@
+require('dotenv').config()
+
 module.exports = {
-  title: 'Book this thing',
-  domain: 'thing.com',
-  timezone: 'Pacific/Auckland',
-  password: 'password',
-  company: 'TCOATS',
-  product: 'BookIT',
-  defaultnames: [
-    'Ben Stiller',
-    'Robin Williams'
-  ],
-  availableTags: [
-    'upstairs',
-    'downstairs'
-  ]
+  title: process.env.BOOKIT_TITLE,
+  domain: process.env.BOOKIT_DOMAIN,
+  timezone: process.env.BOOKIT_TZ,
+  password: process.env.BOOKIT_PASSWORD,
+  company: process.env.BOOKIT_COMPANY,
+  product: process.env.BOOKIT_PRODUCT,
+  defaultnames: process.env.BOOKIT_DEFAULT_NAMES.split('|'),
+  availableTags: process.env.BOOKIT_TAGS.split('|')
 }
